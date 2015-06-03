e58665ad93577b962d7eec0889d98c5eae10e9c6
jjyi <jenny.j.yi@gmail.com>
2015-06-03 14:41:22 -0500

    ActiveAdmin

---
+++ app/admin/journal.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Journal do
+
+permit_params :journal_name, :issues_per_yr,:journal_url,:content_guide_url,:topic_id
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
